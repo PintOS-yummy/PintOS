6a53a6ace993fa121a72395a6f7536225a43c45c
Cho Hyunji <whguswl1007@naver.com>
2023-12-11 14:02:50 +0000

    close 수정

--- userprog/syscall.c
+++ userprog/syscall.c
@@ -8,17 +8,26 @@
 #include "threads/flags.h"
 #include "intrinsic.h"
 #include "threads/init.h" // 추가 power_off()
-#include <string.h> // 추가
+#include <string.h>				// 추가
 // #include "threads/mmu.h" // 추가
-#include <console.h> // 추가
+#include <console.h>			// 추가
 #include "filesys/file.h" // 추가
 
-void syscall_entry (void);
-void syscall_handler (struct intr_frame *);
-void putbuf (const char *buffer, size_t n); // 추가
-void hex_dump (uintptr_t ofs, const void *buf_, size_t size, bool ascii); // 추가
-bool filesys_create (const char *name, off_t initial_size); // 추가
-bool create(const char *file, unsigned initial_size);
+void syscall_entry(void);
+void syscall_handler(struct intr_frame *);
+void putbuf(const char *buffer, size_t n);															 // 추가
+void hex_dump(uintptr_t ofs, const void *buf_, size_t size, bool ascii); // 추가
+bool filesys_create(const char *name, off_t initial_size);							 // 추가
+
+// 추가
+void sys_halt(void);
+void sys_exit(int status);
+int sys_write(int fd, const void *buffer, unsigned size);
+bool sys_create(const char *file, unsigned initial_size);
+struct file *filesys_open(const char *name);
+void open_(const char *name);
+void file_close(struct file *file);
+
 /* System call.
  *
  * Previously system call services was handled by the interrupt handler
@@ -27,7 +36,7 @@ bool create(const char *file, unsigned initial_size);
  *
  * The syscall instruction works by reading the values from the the Model
  * Specific Register (MSR). For the details, see the manual. */
-/* 
+/*
  * 시스템 콜.
  *
  * 이전에는 시스템 콜 서비스가 인터럽트 핸들러에 의해 처리되었습니다
@@ -38,48 +47,46 @@ bool create(const char *file, unsigned initial_size);
  * 자세한 내용은 매뉴얼을 참조하세요.
  */
 
-#define MSR_STAR 0xc0000081         /* Segment selector msr: 시스템 콜을 실행할 때 코드 세그먼트의 기준을 설정합니다. 여기서 사용자 모드와 커널 모드 간의 세그먼트 전환이 정의됩니다.*/
-#define MSR_LSTAR 0xc0000082        /* Long mode SYSCALL target: 시스템 콜이 발생했을 때 실행될 함수(시스템 콜 핸들러)의 주소를 설정 */
+#define MSR_STAR 0xc0000081					/* Segment selector msr: 시스템 콜을 실행할 때 코드 세그먼트의 기준을 설정합니다. 여기서 사용자 모드와 커널 모드 간의 세그먼트 전환이 정의됩니다.*/
+#define MSR_LSTAR 0xc0000082				/* Long mode SYSCALL target: 시스템 콜이 발생했을 때 실행될 함수(시스템 콜 핸들러)의 주소를 설정 */
 #define MSR_SYSCALL_MASK 0xc0000084 /* Mask for the eflags: 시스템 콜이 진행될 때 마스킹될 플래그를 정의합니다. 이 마스킹은 시스템 콜 처리 중에 발생할 수 있는 인터럽트를 방지하기 위해 사용됩니다. */
 
 /*
  * syscall_init 함수는 운영체제의 시스템 콜 인터페이스를 초기화합니다.
- * 이 함수는 시스템 콜 관련 MSR(Model Specific Registers)을 설정하여, 
- * 운영체제가 시스템 콜을 처리할 준비를 합니다. 
- * MSR은 CPU의 특정 동작을 제어하기 위해 사용되는 레지스터입니다. 
- * write_msr 함수는 특정 MSR에 값을 쓰는 데 사용되며, 
- * 이를 통해 해당 MSR이 가리키는 레지스터에 직접 값을 설정할 수 있습니다. 
+ * 이 함수는 시스템 콜 관련 MSR(Model Specific Registers)을 설정하여,
+ * 운영체제가 시스템 콜을 처리할 준비를 합니다.
+ * MSR은 CPU의 특정 동작을 제어하기 위해 사용되는 레지스터입니다.
+ * write_msr 함수는 특정 MSR에 값을 쓰는 데 사용되며,
+ * 이를 통해 해당 MSR이 가리키는 레지스터에 직접 값을 설정할 수 있습니다.
  * 이 초기화 과정은 시스템이 안전하게 시스템 콜을 수행할 수 있도록 구성하는 중요한 단계입니다.
  */
-void
-syscall_init (void) 
+void syscall_init(void)
 {
 	/* 시스템 콜에 대한 MSR 레지스터를 설정합니다.
 	 * 이 때 SEL_UCSEG(사용자 코드 세그먼트 셀렉터)에서 0x10을 빼고 48비트를 왼쪽으로 시프트하고,
-   * SEL_KCSEG(커널 코드 세그먼트 셀렉터)를 32비트 왼쪽으로 시프트합니다. */
-	write_msr(MSR_STAR, ((uint64_t)SEL_UCSEG - 0x10) << 48  |
-			((uint64_t)SEL_KCSEG) << 32);
+	 * SEL_KCSEG(커널 코드 세그먼트 셀렉터)를 32비트 왼쪽으로 시프트합니다. */
+	write_msr(MSR_STAR, ((uint64_t)SEL_UCSEG - 0x10) << 48 |
+													((uint64_t)SEL_KCSEG) << 32);
 
 	/* syscall_entry 함수의 주소를 MSR_LSTAR 레지스터에 씁니다. */
-	write_msr(MSR_LSTAR, (uint64_t) syscall_entry);
+	write_msr(MSR_LSTAR, (uint64_t)syscall_entry);
 
 	/* The interrupt service rountine should not serve any interrupts
 	 * until the syscall_entry swaps the userland stack to the kernel
 	 * mode stack. Therefore, we masked the FLAG_FL. */
 	/* 인터럽트 서비스 루틴은 syscall_entry가 사용자 모드 스택을 커널 모드 스택으로 교환할 때까지
-   * 어떠한 인터럽트도 서비스해서는 안 됩니다. 그래서 FLAG_FL을 마스킹합니다. 
+	 * 어떠한 인터럽트도 서비스해서는 안 됩니다. 그래서 FLAG_FL을 마스킹합니다.
 	 */
 	write_msr(MSR_SYSCALL_MASK,
-			FLAG_IF | FLAG_TF | FLAG_DF | FLAG_IOPL | FLAG_AC | FLAG_NT);
+						FLAG_IF | FLAG_TF | FLAG_DF | FLAG_IOPL | FLAG_AC | FLAG_NT);
 }
 
 /* The main system call interface */
-void
-syscall_handler (struct intr_frame *f) 
+void syscall_handler(struct intr_frame *f)
 {
 	// 락 초기화
-	// 글로벌 락 사용해서 file간 경쟁 조건 피하기->filesystem과 연관된 코드에서 글로벌 락 사용하기 
-	lock_init(&filesys_lock); 
+	// 글로벌 락 사용해서 file간 경쟁 조건 피하기->filesystem과 연관된 코드에서 글로벌 락 사용하기
+	lock_init(&filesys_lock);
 
 	// printf ("\nsystem call!\n");
 	// printf("\nf->R.rax: %d\n", f->R.rax);
@@ -90,76 +97,62 @@ syscall_handler (struct intr_frame *f)
 	 * 전체적인 흐름 - 뇌피셜 가득
 	 * 1. /user/syscall.c에서 레지스터에 정보를 넣고 커널(/userprog/syscall.c)에 전달->문맥전환????
 	 * 2. 커널은 syscall_handler를 호출해서 이게 어떤 시스템 콜인지 파악하고 처리
-	 * 3. 
+	 * 3.
 	 */
 	switch (f->R.rax)
 	{
-	case SYS_HALT:  
-		halt();  
-		break; 
-	case SYS_EXIT:  
-		exit(f->R.rdi);  
-		break; 
-	case SYS_FORK:  /*fork_(f->R.rdi);*/  break; 
-	case SYS_EXEC:  /*exec_(f->R.rdi);*/  break; 
+	case SYS_HALT:
+		sys_halt();
+		break; // 0번
+	case SYS_EXIT:
+		sys_exit(f->R.rdi);
+		break;			 // 1번
+	case SYS_FORK: /*fork_(f->R.rdi);*/
+		break;			 // 2번
+	case SYS_EXEC: /*exec_(f->R.rdi);*/
+		break;			 // 3번
 	case SYS_WAIT:
 		// wait_(f->R.rdi);
 		break;
-	case SYS_CREATE:  
-		f->R.rax = create(f->R.rdi, f->R.rsi);  
-		break; 
+	case SYS_CREATE:
+		f->R.rax = sys_create(f->R.rdi, f->R.rsi);
+		break; // 5번
 	case SYS_REMOVE:
 		// remove_(f->R.rdi);
 		break;
 	case SYS_OPEN:
-		f->R.rax = open(f->R.rdi);
+		f->R.rax = sys_open(f->R.rdi);
 		break;
 	case SYS_FILESIZE:
 		// filesize_(f->R.rdi);
 		break;
 	case SYS_READ:
-		// read_(f->R.rdi, f->R.rsi, f->R.rdx);
+		f->R.rax = sys_read(f->R.rdi, f->R.rsi, f->R.rdx);
 		break;
-	case SYS_WRITE:  f->R.rax = write(f->R.rdi, f->R.rsi, f->R.rdx);  break; // 10번
+	case SYS_WRITE:
+		f->R.rax = sys_write(f->R.rdi, f->R.rsi, f->R.rdx);
+		break; // 10번
 	case SYS_SEEK:
 		// seek_(f->R.rdi, f->R.rsi);
 		break;
 	case SYS_TELL:
 		// tell_(f->R.rdi);
 		break;
-	case SYS_CLOSE:
-		// close_(f->R.rdi);
-		break;
+	case SYS_CLOSE:  sys_close(f->R.rdi);  break;
 	default:
-    printf("존재하지 않는 case\n");
+		printf("존재하지 않는 case\n");
 	}
-	
+
 	// thread_exit ();
 }
 
-void 
-check_page_fault(void* uaddr){
-	struct thread *curr = thread_current();
-	if(uaddr == NULL || is_kernel_vaddr(uaddr) || pml4_get_page(curr->pml4, uaddr) == NULL){
-		exit(-1);
-	}
-}
-static struct file *get_file_fd(int fd){
-	struct thread *curr = thread_current();
-	if(fd<0 || fd>=64){
-		return NULL;
-	}
-	return curr->fdt[fd];
-}
-void 
-halt(void)
+void sys_halt(void)
 {
 	power_off();
 	NOT_REACHED();
 }
 
-void
-exit(int status)
+void sys_exit(int status)
 {
 	struct thread *curr = thread_current();
 	curr->exit_status = status;
@@ -167,54 +160,98 @@ exit(int status)
 	thread_exit();
 }
 
-bool 
-create(const char *file, unsigned initial_size){
+bool sys_create(const char *file, unsigned initial_size)
+{
+
 	check_page_fault(file);
-	bool v = filesys_create(file, initial_size);
-	return v;
+
+	return filesys_create(file, initial_size);
+}
+
+int
+sys_open(const char *name)
+{
+	check_page_fault(name);
+
+	struct thread *curr = thread_current();
+	struct file *get_file = filesys_open(name);
+
+	if (get_file == NULL)
+		return -1;
+
+
+	for (int i = 2; i < (sizeof(curr->fdt) / sizeof(curr->fdt[0])); i++)
+	{
+		if (!curr->fdt[i])
+		{
+			curr->fdt[i] = get_file;
+
+			return i;
+		}
+	}
+
+	return -1;
+}
+
+static struct file 
+*get_file_fd(int fd)
+{
+	struct thread *curr = thread_current();
+
+	if (fd < 0 || fd >= 64)
+		return NULL;
+
+	return curr->fdt[fd];
 }
 
 
-int
-write(int fd, const void *buffer, unsigned size)
-{	
-	int result;
-
+int 
+sys_write(int fd, const void *buffer, unsigned size)
+{
 	check_page_fault(buffer);
+
 	if (fd == 1)
 	{
+		
 		putbuf(buffer, size);
+
 		return size;
 	}
 	else 
 	{
-		if(get_file_fd(fd) == NULL){
-			result = -1;
-		}else{
-			result = file_write(get_file_fd(fd), buffer, size);
+		
+		if (get_file_fd(fd) == NULL) // 추가
+			return -1;
+		else
+		{
+			return file_write(get_file_fd(fd), buffer, size);
 		}
-		return result;
 	}
+
+}
+
+void 
+check_page_fault(void *uadder)
+{
+	struct thread *curr = thread_current(); // va가 pa에 매핑이 되어있는지 확인
+	if (uadder == NULL || is_kernel_vaddr(uadder) || pml4_get_page(curr->pml4, uadder) == NULL)
+	{
+		sys_exit(-1);
+	}
+}
+
+void
+sys_close(int fd){
+
+	if (fd < 0 || fd > 63) return;
+	if (thread_current()->fdt[fd] == NULL) return;
 	
+	file_close(thread_current()->fdt[fd]);
+	thread_current()->fdt[fd] = NULL;
+	return;
 }
 
 int 
-open(const char *file){
+sys_read(int fd, void *buffer, unsigned size){
 
-	check_page_fault(file);
-	struct thread *curr = thread_current();
-	struct file *get_file = filesys_open(file);
-
-	if(get_file == NULL){
-		return -1;
-	}
-
-	for(int i=2; i<64;i++){
-		if(!curr->fdt[i]){
-			curr->fdt[i] = file;
-			return i;
-		}
-	}
-	file_close(get_file);
-	return -1;
 }
